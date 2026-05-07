8d73a615061fc382d6a0a560daa54339b0c7c6ac
Ygor David <ygordavidmedeiros2006@gmail.com>
2026-05-07 08:46:26 -0300

    Alteração na Base da página

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render
+from .models import *
+
+def home_view(request):
+    context = {
+    }
+
+    return render(request,'home.html', context)
