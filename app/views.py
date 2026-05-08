0cbf369f89592cd0315bf43c75303a0a7f503a65
jorgearaujoengquim <jorgearaujoengquim@gmail.com>
2026-05-08 19:22:21 -0300

    feat(auth): implementa sistema de login e navegação dinâmica no base.html

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib import messages
 
 def home_view(request):
     context = {
@@ -11,11 +13,25 @@ def home_view(request):
     return render(request,'home.html', context)
 
 def login_view(request):
-    context = {
-    }
-
-    return render(request,'login.html', context)
-
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            usuario = authenticate(username=username, password=password)
+            
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('home') # Certifique-se que a rota da home se chama 'home' no urls.py
+            else:
+                messages.error(request, "Usuário ou senha inválidos.")
+        else:
+            messages.error(request, "Informações inválidas.")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
+   
 def cadastro_view(request):
     context = {
     }
@@ -23,7 +39,5 @@ def cadastro_view(request):
     return render(request, 'cadastro.html', context)
 
 def logout_view(request):
-    context = {
-    }
-    
-    return render(request, context)
+    logout(request)
+    return redirect('login')
